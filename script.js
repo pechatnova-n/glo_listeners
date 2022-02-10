3f281e9a40f59836d83eb0cee97676d8a8fb1573
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-10 12:23:00 +0300

    working with events

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
+'use strict';
 
+const btn = document.getElementById('btn');
+const e_btn = document.getElementById('e_btn');
+const input = document.getElementById('text');
+const range = document.getElementById('range');
+const range_span = document.getElementById('range-span');
+const square = document.getElementById('square');
+const circle = document.getElementById('circle');
+
+
+
+function changeColor () {
+    let color = input.value;
+    square.style.backgroundColor = color;
+}
+function changeSettings () {
+    let param = range.value;
+    range_span.textContent = param;
+    circle.style.width = `${param}%`;
+    circle.style.height = `${param}%`;
+}
+
+
+btn.addEventListener('click', changeColor);
+range.addEventListener('input', changeSettings);
+
+
+
+
+e_btn.style.display = "none";
 
 
 
